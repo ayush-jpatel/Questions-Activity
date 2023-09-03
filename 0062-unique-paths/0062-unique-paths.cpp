60bd072c2618b0fda48ce3791e1410167b809875
ayush <139282528+ayush-jpatel@users.noreply.github.com>
2023-09-03 19:46:37 +0530

    Time: 3 ms (33.2%), Space: 6.5 MB (54.47%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> vec(m+1,vector<int>(n+1,1));
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                vec[i][j] = vec[i-1][j] + vec[i][j-1];
+            }
+        }
+        return vec[m-1][n-1];
+    }
+};
