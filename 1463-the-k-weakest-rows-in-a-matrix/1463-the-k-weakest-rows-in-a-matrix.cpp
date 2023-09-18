bb4f25b35f2269177d9edfdcbab56af102d47b40
ayush <139282528+ayush-jpatel@users.noreply.github.com>
2023-09-19 02:00:48 +0530

    Time: 8 ms (75.87%), Space: 10.8 MB (9.72%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<mat.size();i++)
         {
             int count = 0;
-            for(int j=0;j<mat[0].size();j++)
+            for(int j=0;j<mat[0].size() && mat[i][j] != 0;j++)
             {
                 count += mat[i][j];
             }
