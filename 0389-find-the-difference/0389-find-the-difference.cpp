399744b8ee7e5ba31315fadf0a0b7282ad984d31
ayush <139282528+ayush-jpatel@users.noreply.github.com>
2023-09-26 01:54:09 +0530

    Time: 0 ms (100%), Space: 7 MB (17.7%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        for(int i=0;i<t.size();i++)
+        {
+            sum = sum + t[i];
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            sum = sum - s[i];
+        }
+        return (char)sum;
+    }
+};
